deb160bb01153697931b339959dcf5debccab50a
jhsui <jiahansui1998@gmail.com>
2025-09-29 13:26:07 -0400

    07 & 08 done.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function (string) {
 
+    const chars = string.split("");
+
+    let result = "";
+
+    for (let i = chars.length - 1; i >= 0; i--) {
+        result += chars[i];
+    }
+
+    return result;
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    for (let i = 0; i < args.length; i++) {
+        while (array.includes(args[i])) {
+
+            let index = array.findIndex(item => item === args[i]);
+            array.splice(index, 1);
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
