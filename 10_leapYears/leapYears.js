ad69f67dd559e3ab01856314f8268855ce4aab54
jhsui <jiahansui1998@gmail.com>
2025-09-29 14:15:11 -0400

    10 & 11.

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,22 @@
-const leapYears = function() {
+const leapYears = function (year) {
+    if (!Number.isInteger(year)) {
+        return false;
+    }
+
+    if (year < 0) {
+        return false;
+    }
+
+    if (year % 4 !== 0) {
+        return false;
+    }
+
+    if (year % 100 === 0 && year % 400 !== 0) {
+        return false;
+    }
+
+    return true;
+
 
 };
 
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,26 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degree) {
+  if (typeof degree !== "number") {
+    return "ERROR";
+  }
+
+  result = (degree - 32) * 5 / 9;
+
+  result = Math.round(result * 10) / 10;
+
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degree) {
+
+  if (typeof degree !== "number") {
+    return "ERROR";
+  }
+
+  result = degree * 9 / 5 + 32;
+
+  result = Math.round(result * 10) / 10;
+
+  return result;
 };
 
 // Do not edit below this line
