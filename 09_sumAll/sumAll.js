a9710d4d34346f6bf1d0e4212388914d6c36457f
jhsui <jiahansui1998@gmail.com>
2025-09-29 13:43:58 -0400

    09.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    if (typeof a !== "number" || typeof b !== "number") {
+        return "ERROR";
+    }
 
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+
+    if (a % 1 !== 0 || b % 1 !== 0) {
+        return "ERROR";
+    }
+
+    let result = 0;
+
+    let small = a > b ? b : a;
+    let large = a > b ? a : b;
+
+    for (let i = small; i <= large; i++) {
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
