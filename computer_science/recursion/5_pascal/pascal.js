6e0073bb0f29ed3b099bcf025fb57c5757a90292
jhsui <jiahansui1998@gmail.com>
2026-01-28 00:42:07 -0500

    optimize recursion_5

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,21 +1,19 @@
 const pascal = function (n) {
-  if (!Number.isInteger(n)) return undefined;
+  if (!Number.isInteger(n) || n < 1) return undefined;
 
-  if (n < 1) return undefined;
+  if (n === 1) return [1];
 
-  if (n === 1) {
-    return [1];
-  }
+  const prev = pascal(n - 1);
 
   const arr = [];
 
-  for (let i = 0; i <= pascal(n - 1).length; i++) {
+  for (let i = 0; i <= prev.length; i++) {
     if (i === 0) {
-      arr[i] = pascal(n - 1)[0];
-    } else if (i === pascal(n - 1).length) {
-      arr[i] = pascal(n - 1)[pascal(n - 1).length - 1];
+      arr[i] = prev[0];
+    } else if (i === prev.length) {
+      arr[i] = prev[prev.length - 1];
     } else {
-      arr[i] = pascal(n - 1)[i - 1] + pascal(n - 1)[i];
+      arr[i] = prev[i - 1] + prev[i];
     }
   }
 
