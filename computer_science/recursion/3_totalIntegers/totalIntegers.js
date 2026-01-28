8a355c9b823448408a64f7a017db58c1195a2f1e
jhsui <jiahansui1998@gmail.com>
2026-01-27 23:30:52 -0500

    complete recursion_3

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,21 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (obj) {
+  if (typeof obj !== "object") {
+    return undefined;
+  }
+  const values = Object.values(obj);
+
+  let count = 0;
+
+  for (const e of values) {
+    if (Number.isInteger(e)) {
+      count += 1;
+    } else if (typeof e === "object" && e !== null) {
+      count += totalIntegers(e);
+    }
+  }
+
+  return count;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
