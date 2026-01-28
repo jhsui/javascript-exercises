67a070e3372790fd5a8a5f90faa737945dc92aaa
jhsui <jiahansui1998@gmail.com>
2026-01-28 00:15:40 -0500

    complete recursion_5

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,26 @@
-const permutations = function() {
-  
+const permutations = function (arr) {
+  if (arr.length === 0) {
+    return [[]];
+  }
+
+  if (arr.length === 1) {
+    return [arr];
+  }
+
+  const whatIsReturned = [];
+  const first = arr[0];
+
+  const subArr = arr.toSpliced(0, 1);
+
+  for (let i = 0; i <= subArr.length; i++) {
+    for (const ar of permutations(subArr)) {
+      const newArr = ar.toSpliced(i, 0, first);
+      whatIsReturned.push(newArr);
+    }
+  }
+
+  return whatIsReturned;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
