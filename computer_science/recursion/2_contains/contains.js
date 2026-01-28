ffccab7aa48fe07091e7b5c82fcf59f81168e9d5
jhsui <jiahansui1998@gmail.com>
2026-01-27 22:30:17 -0500

    complete recursion_2

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,32 @@
-const contains = function() {
-  
+const contains = function (obj, value) {
+  for (const prop in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+      //   if (obj[prop] === value && !isNaN(obj[prop])) {
+      if (obj[prop] === value) {
+        return true;
+        //   } else if (
+        //     typeof obj[prop] !== "string" &&
+        //     Number.isNaN(value) &&
+        //     Number.isNaN(obj[prop])
+        //   ) {
+        //     return true;
+      } else {
+        if (Number.isNaN(value) && Number.isNaN(obj[prop])) {
+          return true;
+        }
+
+        if (typeof obj[prop] === "object" && obj[prop] !== null) {
+          //   return contains(obj[prop], value);
+          if (contains(obj[prop], value)) {
+            return true;
+          }
+        }
+      }
+    }
+  }
+
+  return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
