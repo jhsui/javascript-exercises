3be0134358a8d69aba8486b9745d96e73225f786
jhsui <jiahansui1998@gmail.com>
2026-01-27 19:44:52 -0500

    recursion_1 complete

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,15 @@
-const factorial = function() {
+const factorial = function (n) {
+  if (isNaN(n)) return undefined;
 
+  if (!Number.isInteger(n)) return undefined;
+
+  if (n < 0) return undefined;
+
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 };
 
 // Do not edit below this line
