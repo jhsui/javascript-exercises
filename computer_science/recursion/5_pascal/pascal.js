b6dc85025b52547b7e0d589a12374b910087e423
jhsui <jiahansui1998@gmail.com>
2026-01-28 00:28:00 -0500

    complete recursion_5

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,26 @@
-const pascal = function() {
-  
+const pascal = function (n) {
+  if (!Number.isInteger(n)) return undefined;
+
+  if (n < 1) return undefined;
+
+  if (n === 1) {
+    return [1];
+  }
+
+  const arr = [];
+
+  for (let i = 0; i <= pascal(n - 1).length; i++) {
+    if (i === 0) {
+      arr[i] = pascal(n - 1)[0];
+    } else if (i === pascal(n - 1).length) {
+      arr[i] = pascal(n - 1)[pascal(n - 1).length - 1];
+    } else {
+      arr[i] = pascal(n - 1)[i - 1] + pascal(n - 1)[i];
+    }
+  }
+
+  return arr;
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
